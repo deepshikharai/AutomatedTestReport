42a7aa7a848a16216145d0abc660b0c735136748
pramila-git <36467622+pramila-git@users.noreply.github.com>
2018-10-23 17:36:51 +0530

    updated url

--- testng-test/src/test/java/mytests/SparkMultiBrowserTest.java
+++ testng-test/src/test/java/mytests/SparkMultiBrowserTest.java
@@ -70,7 +70,8 @@ public class SparkMultiBrowserTest {
 			dcp.setCapability("name", "Spark");
 			dcp.setCapability("idleTimeout", 150);
 			//driver = new RemoteWebDriver(new URL("http://35.187.87.222:4444/wd/hub"),dcp);
-			driver = new RemoteWebDriver(new URL("http://104.199.94.18:4444/wd/hub"),dcp);
+			//driver = new RemoteWebDriver(new URL("http://104.199.94.18:4444/wd/hub"),dcp);
+			driver = new RemoteWebDriver(new URL("http://104.197.155.22:4444/wd/hub"),dcp);
 			//driver = new RemoteWebDriver(new URL(pro.getProperty("selenium.url")),dcp);
 		}
 		else if(browser.equalsIgnoreCase("chrome"))
@@ -89,7 +90,7 @@ public class SparkMultiBrowserTest {
 			dcp.setCapability(CapabilityType.SUPPORTS_NETWORK_CONNECTION, true);
 			dcp.setCapability("name", "Spark");
 			dcp.setCapability("idleTimeout", 150);
-			driver = new RemoteWebDriver(new URL("http://104.199.94.18:4444/wd/hub"),dcp);
+			driver = new RemoteWebDriver(new URL("http://104.197.155.22:4444/wd/hub"),dcp);
 			//driver = new RemoteWebDriver(new URL(pro.getProperty("selenium.url")),dcp);
 		}
 		
